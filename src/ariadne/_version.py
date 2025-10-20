bff348579ae9abeeb2fa0177f969881617478fc6
Hunter B <hmbown@gmail.com>
2025-10-19 19:25:22 -0500

    release

--- src/ariadne/_version.py
+++ src/ariadne/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.2.4.dev10+gdaca804cc.d20251019'
-__version_tuple__ = version_tuple = (0, 2, 4, 'dev10', 'gdaca804cc.d20251019')
+__version__ = version = '0.2.5.dev0'
+__version_tuple__ = version_tuple = (0, 2, 5, 'dev0')
 
-__commit_id__ = commit_id = 'gdaca804cc'
+__commit_id__ = commit_id = 'g29a1a2e19'
